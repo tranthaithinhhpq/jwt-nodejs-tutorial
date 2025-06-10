ad434994bc39ef7f45a412acaddbaf841030e5bc
tranthaithinhhpq <tranthaithinhhpq@gmail.com>
2025-06-10 07:35:37 +0700

    video 39 mock test api

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from '../controller/homeController';
+import apiController from '../controller/apiController'
 const router = express.Router();
 /**
  * 
@@ -13,6 +14,7 @@ const initWebRoutes = (app) => {
     router.post("/delete-user/:id", homeController.handleDeleteUser);
     router.get("/update-user/:id", homeController.getUpdateUserPage);
     router.post("/users/update-user", homeController.handleUpdateUser);
+    router.get("/api/test-api", apiController.testApi);
     return app.use("/", router);
 }
 export default initWebRoutes;
