1353a5e097d3c26d1bfe9fae5220af617927d168
tranthaithinhhpq <tranthaithinhhpq@gmail.com>
2025-06-11 11:15:53 +0700

    video 45 validate and create user

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,3 +1,4 @@
+import loginRegisterService from '../service/loginRegisterService';
 const testApi = (req, res) => {
     return res.status(200).json({
         message: 'ok',
@@ -6,7 +7,39 @@ const testApi = (req, res) => {
 
 }
 
-const handleRegister = (req, res) => {
+const handleRegister = async (req, res) => {
+    try {
+        // check required
+        if (!req.body.email || !req.body.phone || !req.body.password)
+            return res.status(200).json({
+                EM: 'Missing required parameters', // error message
+                EC: '1', // error code
+                DT: '', //data
+            })
+        // check password length 
+        if (req.body.password && req.body.password.length < 3)
+            return res.status(200).json({
+                EM: 'Your password must more than 3 leter', // error message
+                EC: '1', // error code
+                DT: '', //data
+            })
+
+        //service: create user
+        let data = await loginRegisterService.registerNewUser(req.body)
+        return res.status(200).json({
+            EM: data.EM, // error message
+            EC: data.EC, // error code
+            DT: '', //data
+        })
+
+    } catch (e) {
+        return res.status(500).json({
+            EM: 'error from server', // error message
+            EC: '-1', // error code
+            DT: '', //data
+        })
+
+    }
     console.log("call me", req.body)
 }
 module.exports = {
