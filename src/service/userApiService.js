ef94ea46ee30a2f6bd54cff6a21d600e34d66b18
tranthaithinhhpq <tranthaithinhhpq@gmail.com>
2025-06-16 16:37:24 +0700

    video 59 complete CRUD

--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -154,19 +154,46 @@ const createNewUser = async (data) => {
 
 const updateUser = async (data) => {
     try {
+        if (!data.groupId) {
+            return {
+                EM: 'Error with empty GroupId',
+                EC: 1,
+                DT: 'group'
+            }
+        }
         let user = await db.User.findOne({
             where: { id: data.id }
         })
         if (user) {
-            user.save({
-
+            //update
+            await user.update({
+                username: data.username,
+                address: data.address,
+                sex: data.sex,
+                groupId: data.groupId
             })
-        } else {
 
+            return {
+                EM: 'Update user succeeds',
+                EC: 0,
+                DT: ''
+
+            }
+        } else {
+            return {
+                EM: 'User not found',
+                EC: 2,
+                DT: ''
+            }
         }
 
     } catch (e) {
         console.log(e);
+        return {
+            EM: 'something wrongs with services',
+            EC: 1,
+            DT: []
+        }
 
     }
 }
