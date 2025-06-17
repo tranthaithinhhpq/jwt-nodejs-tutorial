8bbf3a0d21a16289bee9610491d93b94c5f159b0
tranthaithinhhpq <tranthaithinhhpq@gmail.com>
2025-06-17 14:37:07 +0700

    video 63 init JWT

---
+++ src/middleware/JWTAction.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+import jwt from "jsonwebtoken";
+
+const createJWT = () => {
+    let payload = { name: 'Eric', address: 'ha noi' };
+    let key = process.env.JWT_SECRET;
+    let token = null;
+    try {
+        token = jwt.sign(payload, key);
+        console.log("im token ", token, "S0S");
+    } catch (err) {
+        console.log(err);
+    }
+
+    return token;
+}
+
+const verifyToken = (token) => {
+    let key = process.env.JWT_SECRET;
+    let data = null;
+
+    try {
+        let decoded = jwt.verify(token, key);
+        data = decoded;
+    } catch (err) {
+        console.log(err);
+    }
+
+    return data;
+}
+
+module.exports = {
+    createJWT, verifyToken
+}
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import initApiRoutes from "./routes/api";
 require("dotenv").config();
 import bodyParser from 'body-parser';
 // import connection from "./config/connectDB";
+import { createJWT, verifyToken } from './middleware/JWTAction'
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -18,6 +19,10 @@ configViewEngine(app);
 // config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+createJWT();
+let decodedData = verifyToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRXJpYyIsImFkZHJlc3MiOiJoYSBub2kiLCJpYXQiOjE3NTAxNDUzODR9.vN7Hc-jpv1GjU_cldTsj5fRs3sKwu_Gr-X-jOj0A578")
+console.log(decodedData)
 
 
 //init web routes
